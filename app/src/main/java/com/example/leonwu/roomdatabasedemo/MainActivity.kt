de24f60d1732511cd5532e66a2c6578f10ea2c43
Leon Wu <leon.wu@outware.com.au>
2017-06-23 16:35:33 +1000

    Add delete funtion

--- app/src/main/java/com/example/leonwu/roomdatabasedemo/MainActivity.kt
+++ app/src/main/java/com/example/leonwu/roomdatabasedemo/MainActivity.kt
@@ -10,7 +10,10 @@ import com.example.leonwu.roomdatabasedemo.database.Employee
 import com.example.leonwu.roomdatabasedemo.database.EmployeeDatabase
 import com.example.leonwu.roomdatabasedemo.injection.ActivityModule
 import com.example.leonwu.roomdatabasedemo.injection.DaggerActivityComponent
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -28,7 +31,8 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initialiseDagger()
-        btn_add.onClick { view ->
+        showData()
+        btn_add.onClick {
             controller.addEmployees(Employee(0, first_name_edit_text.text.toString(), last_name_edit_text.text.toString()))
                     .flatMap { unit ->
                         controller.getAllEmployees()
@@ -36,8 +40,38 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({ list ->
                         updateStatus(list)
+                    }, {}, {
+                        Log.d("complete", "complete")
                     })
         }
+
+        btn_query_by_id.onClick {
+            controller.getEmployeeById(query_edit_text.text.toString().toInt())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ list ->
+                        highlightEmployee(list)
+                    }, {}, {
+
+                    })
+        }
+
+        btn_delete.onClick {
+            controller.getEmployeeByName(first_name_edit_text.text.toString(), last_name_edit_text.text.toString())
+                    .flatMap { controller.deleteEmployee(it) }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        showData()
+                    })
+        }
+    }
+
+    fun showData() {
+        controller.getAllEmployees()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    list ->
+                    updateStatus(list)
+                })
     }
 
     fun initialiseDagger() {
@@ -50,10 +84,25 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     fun updateStatus(list: List<Employee>) {
         val sb: StringBuilder = StringBuilder()
         for (em in list) {
+            sb.append("\n")
             sb.append(em.toString())
             sb.append("\n")
         }
         database_status.text = sb.toString()
+    }
+
+    fun highlightEmployee(list: List<Employee>) {
+        val sb: StringBuilder = StringBuilder()
+        for (em in list) {
+            sb.append("\n")
+            sb.append(em.toString())
+            sb.append("\n")
+        }
+        if (list.size == 0) {
+            query_result.text = getString(R.string.no_result)
+        } else {
+            query_result.text = sb.toString()
+        }
     }
 
 }
--- app/src/main/java/com/example/leonwu/roomdatabasedemo/controller/EmployeeController.kt
+++ app/src/main/java/com/example/leonwu/roomdatabasedemo/controller/EmployeeController.kt
@@ -2,11 +2,9 @@ package com.example.leonwu.roomdatabasedemo.controller
 
 import com.example.leonwu.roomdatabasedemo.database.Employee
 import com.example.leonwu.roomdatabasedemo.database.EmployeeDatabase
-import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
-import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Scheduler
+import io.reactivex.*
 import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Publisher
 
 /**
  * Created by leonwu on 22/6/17.
@@ -39,14 +37,38 @@ class EmployeeController(private val db: EmployeeDatabase) {
         }).subscribeOn(Schedulers.io())
     }
 
-//    fun getEmployeeById(id: Int): Observable<List<Employee>>? {
-//        return Observable.create(object :ObservableOnSubscribe<List<Employee>>{
-//            override fun subscribe(e: ObservableEmitter<Employee>?) {
-//                db.employeeDao().loadAllByIds(arrayOf(id))
-//                e?.onNext()
-//                e?.onComplete()
-//            }
-//
-//        })
-//    }
+    fun getEmployeeById(id: Int): Observable<List<Employee>> {
+        return Observable.create(object : ObservableOnSubscribe<List<Employee>> {
+            override fun subscribe(e: ObservableEmitter<List<Employee>>?) {
+                val list = db.employeeDao().loadAllByIds(arrayOf(id))
+                e?.onNext(list)
+                e?.onComplete()
+            }
+
+        }).subscribeOn(Schedulers.io())
+    }
+
+    fun getEmployeeByName(first: String, last: String): Observable<Employee> {
+        return Observable.create<Employee> { e ->
+            try {
+                val employee = db.employeeDao().findByName(first, last)
+                e.onNext(employee)
+                e.onComplete()
+            } catch (ex: Exception) {
+                e.onError(ex)
+            }
+        }.subscribeOn(Schedulers.io())
+    }
+
+    fun deleteEmployee(employee: Employee): Observable<Unit> {
+        return Observable.create<Unit> { e ->
+            try {
+                db.employeeDao().delete(employee)
+                e.onNext(Unit)
+                e.onComplete()
+            } catch (exception: Exception) {
+                e.onError(exception)
+            }
+        }.subscribeOn(Schedulers.io())
+    }
 }
